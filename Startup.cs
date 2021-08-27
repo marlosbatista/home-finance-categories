81f3556dfeff79d5f598752a3f83d1a041a91e86
Marlos Batista <marlosbatista@gmail.com>
2021-08-27 09:16:02 -0300

    Adicionando Secrets e Healthchecks

--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,13 @@
+using System;
+using System.Linq;
+using System.Net.Mime;
+using System.Text.Json;
 using home_finance_categories.Repositories;
 using home_finance_categories.Settings;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,11 +33,11 @@ namespace home_finance_categories
         {
             BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
             BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+            var mongoDbSettings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
 
             services.AddSingleton<IMongoClient>(ServiceProvider =>
             {
-                var settings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
-                return new MongoClient(settings.ConnectionString);
+                return new MongoClient(mongoDbSettings.ConnectionString);
             });
 
             services.AddSingleton<ICategoriaRepository, MongoDbCategoriaRepository>();
@@ -44,6 +50,14 @@ namespace home_finance_categories
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "home_finance_categories", Version = "v1" });
             });
+
+            services.AddHealthChecks()
+                    .AddMongoDb(
+                        mongoDbSettings.ConnectionString, 
+                        name: "mongodb", 
+                        timeout: TimeSpan.FromSeconds(3),
+                        tags: new [] { "ready" }
+                    );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -65,6 +79,33 @@ namespace home_finance_categories
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                
+                endpoints.MapHealthChecks("/health/ready", new HealthCheckOptions
+                {
+                    Predicate = (check) => check.Tags.Contains("ready"),
+                    ResponseWriter = async(context, report) => 
+                    {
+                        var result = JsonSerializer.Serialize(
+                            new {
+                                status = report.Status.ToString(),
+                                checks = report.Entries.Select(entry => new {
+                                    name = entry.Key,
+                                    status = entry.Value.Status.ToString(),
+                                    exception = entry.Value.Exception != null ? entry.Value.Exception.Message : "none",
+                                    duration = entry.Value.Duration.ToString()
+                                })
+                            }
+                        );
+
+                        context.Response.ContentType = MediaTypeNames.Application.Json;
+                        await context.Response.WriteAsync(result);
+                    }
+                });
+
+                endpoints.MapHealthChecks("/health/live", new HealthCheckOptions
+                {
+                    Predicate = (_) => false
+                });
             });
         }
     }
